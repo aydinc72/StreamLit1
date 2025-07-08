5f5b42fb5866888ecf3853e033bcbc2f181e1d73
aydinc72 <42252381+aydinc72@users.noreply.github.com>
2025-07-08 17:47:10 +0300

    Update App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("Basit Streamlit Uygulamasi")
  
 name = st.text_input("Ad girin:")
-number = st.slider("sayi seçin:", 0, 100, 50)
+number = st.slider("sayi secin:", 0, 100, 50)
  
 if st.button("Goster"):
     st.write(f"Merhaba {name},  sayi {number}.")
