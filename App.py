f8048ec608ab46843a69a7ed00b4255e221cb49f
aydinc72 <42252381+aydinc72@users.noreply.github.com>
2025-07-08 21:40:24 +0300

    Update App.py

--- App.py
+++ App.py
@@ -6,10 +6,15 @@ st.title("Basit Streamlit Uygulamasi")
 name = st.text_input("Ad girin:")
 #number = st.slider("sayi secin:", 0, 100, 50)
 
-if st.button("Goster"):
-    if (name.upper().startswith("AYD")):
-        st.write(f"Merhaba super {name}")
-    if (name.upper().startswith("MEL")):
-        st.write(f"Merhaba gıcık {name}")
-    if (not name.upper().startswith("MEL") and not name.upper().startswith("AYD")):
-        st.write(f"Merhaba normal {name}")
+if st.button("Göster"):
+    if name:  # Boş giriş kontrolü
+        name_upper = name.upper()
+
+        if name_upper.startswith("AYD"):
+            st.write(f"Merhaba süper {name}")
+        elif name_upper.startswith("MEL"):
+            st.write(f"Merhaba gıcık {name}")
+        else:
+            st.write(f"Merhaba normal {name}")
+    else:
+        st.warning("Lütfen bir isim girin.")
