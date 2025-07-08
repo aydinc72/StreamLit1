4a9ad5c4ef41804246221228f5da81c75aa70d5a
aydinc72 <42252381+aydinc72@users.noreply.github.com>
2025-07-08 21:29:46 +0300

    Update App.py

--- App.py
+++ App.py
@@ -7,4 +7,9 @@ name = st.text_input("Ad girin:")
 number = st.slider("sayi secin:", 0, 100, 50)
 
 if st.button("Goster"):
-    st.write(f" sayi {number * number * 3.14}.")
+    if (name.upper().startswith("AYD")):
+        st.write(f"Merhaba super {name}")
+    if (name.upper().startswith("MEL")):
+        st.write(f"Merhaba gıcık {name}")
+    if (not name.upper().startswith("MEL") and not name.upper().startswith("AYD")):
+        st.write(f"Merhaba normal {name}")
